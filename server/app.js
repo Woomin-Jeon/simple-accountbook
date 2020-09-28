698b6c3d2e805bbd7a3e9e2934969a030c0e96e0
woomin <dal96k@gmail.com>
2020-09-28 12:53:36 +0900

    [Init] 프로젝트 백엔드 기본 환경 설정
    프로젝트의 시작에 앞서 eslint, express와 같은 기본 환경을
    설정하였습니다.

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.static('../client'));
+
+app.get('/', (req, res, next) => {
+  res.render('index.html');
+});
+
+app.use('/', require('./routes'));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Error handler catches server error');
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
+});
