5a686b853011eff2d63b473eacbddf7b117fe4fe
woomin <dal96k@gmail.com>
2020-10-05 20:01:22 +0900

    [Refactor] 중복되는 로직 제거, 쿼리문 간결화
    중복되었던 if(error) reject(error) 문을 util.js에 따로 함수로 분리하여서
    checkError라는 함수로 사용하였습니다. 아울러 쿼리문이 너무 길어서
    가독성이 저해되는 것 같아서, 이를 개행을 주어 그나마 눈에 잘 들어오도록
    하였습니다.

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,21 +1,20 @@
 const pool = require('./index');
 
+const { checkError } = require('./util');
+
 const checkPassword = (id, pw) => new Promise((resolve, reject) => {
   pool.getConnection((error, connection) => {
-    const query = `SELECT id, pw FROM user WHERE id=?`;
-    connection.query(query, [id], (error, data) => {
-      if (error) {
-        reject(error);
-      }
+    const query = `
+      SELECT id, pw 
+      FROM user 
+      WHERE id=?`;
+    const params = [id];
+
+    connection.query(query, params, (error, data) => {
+      checkError(error, reject);
 
       const [user] = data;
-
-      if (!user || user.pw !== pw) {
-        resolve(null);
-        return;
-      }
-
-      resolve({ ...user });
+      user && user.pw === pw ? resolve({ ...user }) : resolve(null);
     });
 
     connection.release();
--- server/models/breakdown.js
+++ server/models/breakdown.js
@@ -1,30 +1,40 @@
 const pool = require('./index');
 
+const { checkError } = require('./util');
+
 const getBreakdowns = (userId) => new Promise((resolve, reject) => {
   pool.getConnection((error, connection) => {
-    const query = `SELECT breakdown.id, breakdown.amount, breakdown.content, breakdown.method, breakdown.come, date, category.name FROM breakdown LEFT JOIN category ON breakdown.categoryId = category.id WHERE userId=?`;
-    connection.query(query, [userId], (error, data) => {
-      if (error) {
-        reject(error);
-      }
+    const query = `
+      SELECT
+      breakdown.id, amount, content, method, breakdown.come, date, category.name as category
+      FROM breakdown
+      LEFT JOIN category ON breakdown.categoryId = category.id
+      WHERE userId=?
+    `;
+    const params = [userId];
 
+    connection.query(query, params, (error, data) => {
+      checkError(error, reject);
       resolve(data);
     });
 
     connection.release();
   });
-});
+}).catch(error => console.error(error));
 
 const addBreakdown = ({
   id, amount, content, method, come, date, userId, categoryId,
 }) => new Promise((resolve, reject) => {
   pool.getConnection((error, connection) => {
-    const query = `INSERT INTO breakdown (id, amount, content, method, come, date, userId, categoryId) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-    connection.query(query, [id, amount, content, method, come, date, userId, categoryId], (error) => {
-      if (error) {
-        reject(error);
-      }
+    const query = `
+      INSERT INTO breakdown
+      (id, amount, content, method, come, date, userId, categoryId)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    const params = [id, amount, content, method, come, date, userId, categoryId];
 
+    connection.query(query, params, (error) => {
+      checkError(error, reject);
       resolve(true);
     });
 
@@ -36,15 +46,14 @@ const updateBreakdown = ({
   breakdownId, amount, content, method, come, date, userId, categoryId,
 }) => new Promise((resolve, reject) => {
   pool.getConnection((error, connection) => {
-    const query = `UPDATE breakdown SET
-      amount=?, content=?, method=?, come=?, date=?, categoryId=?
+    const query = `
+      UPDATE breakdown
+      SET amount=?, content=?, method=?, come=?, date=?, categoryId=?
       WHERE id=? and userId=?`;
     const params = [amount, content, method, come, date, categoryId, breakdownId, userId];
-    connection.query(query, params, (error) => {
-      if (error) {
-        reject(error);
-      }
 
+    connection.query(query, params, (error) => {
+      checkError(error, reject);
       resolve(true);
     });
 
@@ -54,12 +63,14 @@ const updateBreakdown = ({
 
 const deleteBreakdown = (userId, breakdownId) => new Promise((resolve, reject) => {
   pool.getConnection((error, connection) => {
-    const query = `DELETE FROM breakdown WHERE userId=? and id=?`;
-    connection.query(query, [userId, breakdownId], (error) => {
-      if (error) {
-        reject(error);
-      }
+    const query = `
+      DELETE FROM breakdown
+      WHERE userId=? and id=?
+    `;
+    const params = [userId, breakdownId];
 
+    connection.query(query, params, (error) => {
+      checkError(error, reject);
       resolve(true);
     });
 
