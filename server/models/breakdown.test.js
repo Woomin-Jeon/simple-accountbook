c558cc4f9b4d0f37e9474b7f605283a5222ab4ec
woomin <dal96k@gmail.com>
2020-10-05 19:39:41 +0900

    [Refactor] breakdown.test.js에서 공통되는 변수 추출
    테스트에서 공통적으로 쓰이는 변수들을 추출하여 관리하였으며, 이 과정에서
    필요없어진 getRandomString() 함수는 제거하였습니다.

--- server/models/breakdown.test.js
+++ server/models/breakdown.test.js
@@ -2,54 +2,44 @@ const {
   getBreakdowns, addBreakdown, deleteBreakdown, updateBreakdown,
 } = require('./breakdown');
 
-const { getRandomString } = require('../utils/generator');
-
 describe('breakdown', () => {
-  let breakdownId;
+  const userId = 'test';
+  const breakdownId = 'Random_breakdown_id';
+  const data = {
+    id: breakdownId,
+    amount: 30_000,
+    content: '친구들과 술 한잔',
+    method: '카드',
+    come: '지출',
+    date: '2020-10-05',
+    userId,
+    categoryId: 5,
+  };
+  const updatedData = {
+    ...data,
+    amount: 20_000,
+    content: '친구들과 밥 한끼',
+    date: '2020-10-04',
+  };
+
+  describe('addBreakdown', () => {
+    it('returns true', async () => {
+      const result = await addBreakdown(data);
+
+      expect(result).toBe(true);
+    });
+  });
 
   describe('getBreakdowns', () => {
     it('returns breakdowns', async () => {
-      const userId = 'boost';
       const breakdowns = await getBreakdowns(userId);
 
       expect(breakdowns.length > 0).toBe(true);
     });
   });
 
-  describe('addBreakdown', () => {
-    it('returns true', async () => {
-      const parameters = {
-        id: getRandomString(),
-        amount: 30_000,
-        content: '친구들과 술 한잔',
-        method: '카드',
-        come: '지출',
-        date: '2020-10-05',
-        userId: 'test',
-        categoryId: 5,
-      };
-
-      const result = await addBreakdown(parameters);
-
-      expect(result).toBe(true);
-
-      breakdownId = parameters.id;
-    });
-  });
-
   describe('updateBreakdown', () => {
     it('returns true', async () => {
-      const updatedData = {
-        id: breakdownId,
-        amount: 20_000,
-        content: '친구들과 밥 한끼',
-        method: '카드',
-        come: '지출',
-        date: '2020-10-05',
-        userId: 'test',
-        categoryId: 5,
-      };
-
       const result = await updateBreakdown(updatedData);
 
       expect(result).toBe(true);
@@ -58,7 +48,6 @@ describe('breakdown', () => {
 
   describe('deleteBreakdown', () => {
     it('returns true', async () => {
-      const userId = 'test';
       const result = await deleteBreakdown(userId, breakdownId);
 
       expect(result).toBe(true);
