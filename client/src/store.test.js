a8ea331ef0850a5c1f7eabdacf45380953e7f0b3
woomin <dal96k@gmail.com>
2020-10-06 21:06:37 +0900

    [Test] setTab 함수에 대한 테스트 작성
    setTab 함수가 잘 작동하는지 확인하기 위해 테스트를 작성하였습니다.

--- client/src/store.test.js
+++ client/src/store.test.js
@@ -6,14 +6,26 @@ describe('setMonth', () => {
   });
 
   describe('with next parameter', () => {
-    it('returns month + 1', () => {
+    it('makes month + 1', () => {
       expect(actions.setMonth('next')).toEqual({ month: 4 });
     });
   });
 
   describe('with previous parameter', () => {
-    it('returns month - 1', () => {
+    it('makes month - 1', () => {
       expect(actions.setMonth('prev')).toEqual({ month: 2 });
+    });
+  });
+});
+
+describe('setTab', () => {
+  beforeEach(() => {
+    store.state = { tab: '내역' };
+  });
+
+  describe('with clicked another tab', () => {
+    it('makes tab state that another tab', () => {
+      expect(actions.setTab('통계')).toEqual({ tab: '통계' });
     });
   });
 });
