8efceae5a4e42729c018d2d7e02c35919729daa8
woomin <dal96k@gmail.com>
2020-10-07 15:18:28 +0900

    [Feat] 유틸 함수 생성
    편하게 사용하기 위한 유틸 함수들을 util.js에 모아서 만들었습니다.
    getCurrentMonth, getCurrentDate와 같은 함수를 만들었습니다.

---
+++ client/src/util.js
@@ -0,0 +1,17 @@
+export const getCurrentDate = () => {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, 0);
+  const date = today.getDate().toString().padStart(2, 0);
+
+  return `${year}-${month}-${date}`;
+};
+
+export const getCurrentMonth = () => {
+  const today = new Date();
+
+  const month = (today.getMonth() + 1).toString().padStart(2, 0);
+
+  return Number(month);
+};
