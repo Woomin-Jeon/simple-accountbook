a16d16343368d7b538526a7e6df0c3631d4635f4
woomin <dal96k@gmail.com>
2020-10-14 13:33:33 +0900

    [Feat] 날짜 별 금액 총계 출력하는 기능 추가
    날짜 별 금액 총계를 출력하는 기능을 추가하였습니다.

--- client/src/components/breakdown/ListDate.js
+++ client/src/components/breakdown/ListDate.js
@@ -1,3 +1,7 @@
+import { store } from '@/store.js';
+
+import { getTotalCome } from '@/util.js';
+
 import ListItem from './ListItem.js';
 
 export default function ListDate(date, items) {
@@ -5,7 +9,13 @@ export default function ListDate(date, items) {
 
   this.render = () => {
     this.node.innerHTML = `
-      <div class='list_date'>${date}</div>
+      <div class='space-between'>
+        <div class='list_date'>${date}</div>
+        <div class='list_date_result flex'>
+          <div class='list_date_result_income'>+${getTotalCome('수입', items)}원</div>
+          <div class='list_date_result_outcome'>-${getTotalCome('지출', items)}원</div>
+        </div>
+      </div>
       <div>
         ${items.map(item => `
           <div>${new ListItem(item).node.innerHTML}</div>
