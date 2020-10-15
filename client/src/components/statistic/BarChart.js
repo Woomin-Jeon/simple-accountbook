83c5b944ce5ba8ee775a52ca1a3df3a2664f8981
woomin <dal96k@gmail.com>
2020-10-15 16:06:57 +0900

    [Feat] 막대 차트 구현 완료
    유저의 지출 내역을 카테고리 별로 막대그래프로 그려주는 컴포넌트를 구현
    완료하였습니다.

---
+++ client/src/components/statistic/BarChart.js
@@ -0,0 +1,25 @@
+import { store, observer } from '@/store.js';
+
+import { splitByCategory } from '@/util.js';
+
+import Bar from './Bar.js';
+
+export default function BarChart() {
+  this.node = document.createElement('div');
+  this.node.classList.add('bar_chart');
+
+  this.render = () => {
+    const outcomeBreakdowns = store.breakdown.items.filter(item => item.come === '지출');
+    const costByCategories = splitByCategory(outcomeBreakdowns);
+
+    this.node.innerHTML = `<div id='bar_chart'></div>`;
+    const barChart = document.querySelector('#bar_chart');
+
+    costByCategories.forEach((category, index) => {
+      barChart.appendChild(new Bar(category, index).node);
+    });
+  };
+
+  this.render();
+  observer.subscribe('/statistic', this.render);
+}
--- client/src/pages/StatisticPage.js
+++ client/src/pages/StatisticPage.js
@@ -2,6 +2,7 @@ import Header from '@header/Header.js';
 import MonthToggle from '@header/MonthToggle.js';
 import Tab from '@header/Tab.js';
 import PieChart from '@statistic/PieChart.js';
+import BarChart from '@statistic/BarChart.js';
 
 export default function StatisticPage() {
   this.node = document.createElement('div');
@@ -10,4 +11,5 @@ export default function StatisticPage() {
   this.node.appendChild(new MonthToggle().node);
   this.node.appendChild(new Tab().node);
   this.node.appendChild(new PieChart({ radius: 150 }).node);
+  this.node.appendChild(new BarChart().node);
 }
