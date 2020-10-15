5b07c738540da800e86eb84b376cf053a169f9dd
woomin <dal96k@gmail.com>
2020-10-15 17:07:37 +0900

    [Feat] 통계 페이지의 차트 헤더 컴포넌트 구현
    통계 페이지에서 이번 달 지출 내역과 차트를 변경할 수 있는 라디오 버튼을
    포함한 컴포넌트를 구현하였습니다.

---
+++ client/src/components/statistic/ChartHeader.js
@@ -0,0 +1,28 @@
+import { store, observer } from '@/store.js';
+
+import { getTotalCome } from '@/util.js';
+
+export default function ChartHeader() {
+  this.node = document.createElement('div');
+  this.node.classList.add('space-between');
+  this.node.classList.add('chart_header');
+
+  this.render = () => {
+    this.node.innerHTML = `
+      <div>
+        <label>
+          <input type='radio' name='chart_type' checked />
+          <span>카테고리별 지출</span>
+        </label>
+        <label>
+          <input type='radio' name='chart_type' />
+          <span>일별 지출</span>
+        </label>
+      </div>
+      <div>이번 달 지출 금액 ${getTotalCome('지출', store.breakdown.items)}원</div>
+    `;
+  };
+
+  this.render();
+  observer.subscribe('/statistic', this.render);
+}
--- client/src/pages/StatisticPage.js
+++ client/src/pages/StatisticPage.js
@@ -1,6 +1,7 @@
 import Header from '@header/Header.js';
 import MonthToggle from '@header/MonthToggle.js';
 import Tab from '@header/Tab.js';
+import ChartHeader from '@statistic/ChartHeader.js';
 import PieChart from '@statistic/PieChart.js';
 import BarChart from '@statistic/BarChart.js';
 
@@ -10,6 +11,7 @@ export default function StatisticPage() {
   this.node.appendChild(new Header().node);
   this.node.appendChild(new MonthToggle().node);
   this.node.appendChild(new Tab().node);
+  this.node.appendChild(new ChartHeader().node);
   this.node.appendChild(new PieChart({ radius: 150 }).node);
   this.node.appendChild(new BarChart().node);
 }
